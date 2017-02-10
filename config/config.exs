2b18cdfed1fcd08b99cbe97a320c198fa997ebb5
Gabriel Poblete <gpoblete@acid.cl>
2017-02-10 14:57:24 -0300

    Refix api key

--- config/config.exs
+++ config/config.exs
@@ -38,5 +38,5 @@ config :coherence,
 
 config :coherence, Chatter.Coherence.Mailer,
   adapter: Swoosh.Adapters.Sendgrid,
-  api_key: {:system, "SENDGRID_API_KEY"}
+  api_key: {:system, "API_KEY"}
 # %% End Coherence Configuration %%
