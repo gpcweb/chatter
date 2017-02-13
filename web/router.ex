bd9615b11dd00610cc9e24ccf26689d71ba5aabc
Gabriel Poblete <gpoblete@acid.cl>
2017-02-13 09:45:36 -0300

    Fix routes

--- web/router.ex
+++ web/router.ex
@@ -11,16 +11,39 @@ defmodule Chatter.Router do
     plug Coherence.Authentication.Session
   end
 
+  pipeline :protected do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session, protected: true
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
   end
 
+  scope "/" do
+    pipe_through :browser
+    coherence_routes
+  end
+
+  scope "/" do
+    pipe_through :protected
+    coherence_routes :protected
+  end
+
   scope "/", Chatter do
     pipe_through :browser # Use the default browser stack
-    coherence_routes()
 
-    get "/", PageController, :index
-    get "/welcome", PageController, :welcome
+    get "/", WelcomeController, :index
+  end
+
+  scope "/", Chatter do
+    pipe_through :protected
+
+    get "/lobby", ChattController, :lobby
   end
 
   # Other scopes may use custom stacks.
