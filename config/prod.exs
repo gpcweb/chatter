55a6c238d121141aa120b8e0243ce46ddaed753f
Gabriel Poblete <gpoblete@acid.cl>
2017-02-20 10:31:48 -0300

    Add distillery config to deploy on amazon ec2

--- config/prod.exs
+++ config/prod.exs
@@ -13,20 +13,38 @@ use Mix.Config
 # which you typically run after static files are built.
 config :chatter, Chatter.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "scout-chatter.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: {:system, "APP_HOST"}, port: {:system, "PORT"}],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  server: true,
+  root: ".",
+  version: Mix.Project.config[:version],
+  secret_key_base: {:system, "SECRET_KEY_BASE"}
+
+
+# config :chatter, Chatter.Endpoint,
+#   http: [port: {:system, "PORT"}],
+#   url: [scheme: "https", host: "scout-chatter.herokuapp.com", port: 443],
+#   force_ssl: [rewrite_on: [:x_forwarded_proto]],
+#   cache_static_manifest: "priv/static/manifest.json",
+#   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
 
 # Configure your database
- config :chatter, Chatter.Repo,
-   adapter: Ecto.Adapters.Postgres,
-   url: System.get_env("DATABASE_URL"),
-   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-   ssl: true
+ # config :chatter, Chatter.Repo,
+ #   adapter: Ecto.Adapters.Postgres,
+ #   url: System.get_env("DATABASE_URL"),
+ #   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+ #   ssl: true
+
+config :chatter, Chatter.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: {:system, "DATABASE_NAME"},
+  hostname: {:system, "DATABASE_HOST"},
+  username: {:system, "DATABASE_USERNAME"},
+  password: {:system, "DATABASE_PASSWORD"},
+  pool_size: 10
 
 # ## SSL Support
 #
---
+++ rel/config.exs
@@ -0,0 +1,44 @@
+# Import all plugins from `rel/plugins`
+# They can then be used by adding `plugin MyPlugin` to
+# either an environment, or release definition, where
+# `MyPlugin` is the name of the plugin module.
+Path.join(["rel", "plugins", "*.exs"])
+|> Path.wildcard()
+|> Enum.map(&Code.eval_file(&1))
+
+use Mix.Releases.Config,
+    # This sets the default release built by `mix release`
+    default_release: :default,
+    # This sets the default environment used by `mix release`
+    default_environment: Mix.env()
+
+# For a full list of config options for both releases
+# and environments, visit https://hexdocs.pm/distillery/configuration.html
+
+
+# You may define one or more environments in this file,
+# an environment's settings will override those of a release
+# when building in that environment, this combination of release
+# and environment configuration is called a profile
+
+environment :dev do
+  set dev_mode: true
+  set include_erts: false
+  set cookie: :"(C,&NhgK3V*f[$Uo=i=cTxiFk>rG`M%|b|b44!B*[]fym;j8mE!uW<9aLBs!Ij<`"
+end
+
+environment :prod do
+  set include_erts: true
+  set include_src: false
+  set cookie: :"DjVf@!H7l4CQ,1*GVE{K&U^At07@fmk.h}%(>]dolr|Oz?3:9p|*StV|!4&:O$ul"
+end
+
+# You may define one or more releases in this file.
+# If you have not set a default release, or selected one
+# when running `mix release`, the first release in the file
+# will be used by default
+
+release :chatter do
+  set version: current_version(:chatter)
+end
+
